3eabc14184b371893eef6dce051f03b042e68472
wilkens95 <107919396+wilkens95@users.noreply.github.com>
2024-07-24 11:10:11 +0200

    Add files via upload

---
+++ THE ARTBOX/oeuvres-1.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>The ArtBox</title>
+</head>
+<body>
+<header>
+    <a href="index.php"><img src="img/logo.png" alt="Logo Artbox" id="logo"></a>
+    <nav>
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+        </ul>
+    </nav>
+</header>
+<main>
+    <article id="detail-oeuvre">
+        <div id="img-oeuvre">
+            <img src="img/clark-van-der-beken.png" alt="Dodomu">
+        </div>
+        <div id="contenu-oeuvre">
+            <h1>Dodomu</h1>
+            <p class="description">Mia Tozerski</p>
+            <p class="description-complete">
+                Mia Tozerski est une artiste peintre ukrainienne réfugiée de la guerre. Sur cette œuvre, Dodomu ("domicile" en ukrainien), elle nous montre la tristesse du peuple ukrainien qu'elle partage, ayant elle-même dû quitter son foyer. L'œuvre évoque le drapeau liquéfié d'une Ukraine en souffrance, pleurant la mort de ses compatriotes. Ce travail chargé d'émotion est le symbole d'un événement qui marquera l'Histoire. Cette peinture à l'acrylique rayonne grâce à son fond lisse et ses mélanges de couleurs éclatantes.
+            </p>
+        </div>
+    </article>
+</main>
+<footer>
+    <p>
+        <strong>© THE ARTBOX</strong> - <em>Tous droits réservés</em>
+    </p>
+</footer>
+</body>
+</html>
